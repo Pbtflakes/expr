58b2f983fcd4436d0cfc71bb51baf42d29c4281d
Matthew Lavin <pbtflakes@protonmail.ch>
2017-05-16 21:28:23 -0400

    init

---
+++ expr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+union Calc {
+	char c;
+	long int i;
+};
+
+typedef union Calc calc;
+
+int
+main(int argc, char **argv) {
+	if (argc == 1)
+		return 1;
+	calc *stack = (calc *) calloc(argc - 1, sizeof(calc));
+	free(stack);
+	return 0;
+}
